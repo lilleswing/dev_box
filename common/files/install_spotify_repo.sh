6d1e91ac6930be201c1917de0b31059084fcf964
Karl Leswing <lilleswing@gmail.com>
2015-10-27 11:15:50 -0400

    added spotify and vlc

---
+++ common/files/install_spotify_repo.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D2C19886
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
