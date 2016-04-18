ca5e0cb6ebd6ea73ff4ef31912114e162b646d06
Karl Leswing <lilleswing@gmail.com>
2016-04-18 09:34:03 -0400

    More issues with docker

--- common/files/install_docker_repo.sh
+++ common/files/install_docker_repo.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
