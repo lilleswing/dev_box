e494df091a77426e8f72241ad2ec522fdd748b94
Karl Leswing <lilleswing@gmail.com>
2016-06-26 16:25:11 -0400

    install_jenkins

--- common/files/install_jenkins.sh
+++ common/files/install_jenkins.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -q -O - http://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
-sudo echo "deb http://pkg.jenkins.io/debian binary" >> /etc/apt/sources.list
+sudo echo "deb http://pkg.jenkins.io/debian binary/" >> /etc/apt/sources.list
