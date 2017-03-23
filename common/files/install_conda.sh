9f4e3e23d10d6333a436817e0325cb0b02fbeecb
leswing <Karl>
2017-03-23 11:40:40 -0700

    More conda work

--- common/files/install_conda.sh
+++ common/files/install_conda.sh
@@ -3,3 +3,8 @@
 cd /tmp
 wget $1 -O anaconda.sh
 bash anaconda.sh -b -p $HOME/anaconda3
+
+export PATH="$HOME/anaconda3/bin:$PATH"
+conda create -y --name daily
+conda update conda
+conda update anaconda
